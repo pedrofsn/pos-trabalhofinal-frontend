0fa992ea2cab59f1707c8ae85ba56078e6c74962
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2017-10-28 18:17:20 -0200

    Cadastro de certidões

--- editar.js
+++ editar.js
@@ -53,5 +53,24 @@ $(document).ready(
                 $('#tableIdentificador').append(html);
             });
 
+        $('#button-adicionar-certidao').click(
+            function () {
+                var tipoCertidao = $("#tipoCertidao option:selected").text();
+                var nomeCartorio = $('#nomeCartorio').val();
+                var livro = $('#livro').val();
+                var folha = $('#folha').val();
+                var termo = $('#termo').val();
+
+                var html = '<tr>' +
+                    '<td>' + tipoCertidao + '</td>' +
+                    '<td>' + nomeCartorio + '</td>' +
+                    '<td>' + livro + '</td>' +
+                    '<td>' + folha + '</td>' +
+                    '<td>' + termo + '</td>'
+                    + '</tr>';
+
+                $('#tableCertidao').append(html);
+            });
+
     }
 );
