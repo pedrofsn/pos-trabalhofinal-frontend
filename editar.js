d8fc0032272645cc1cf1679aef142563d8b413ea
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2017-10-28 17:51:31 -0200

    Cadastrando nomes

--- editar.js
+++ editar.js
@@ -1,15 +1,37 @@
 $(document).ready(
     function () {
+
+        $('#nome, #sobrenome, #titulo, #sufixo').on('input',
+            function (e) {
+                var rotulo = $('#nome-completo').val();
+                var titulo = $('#titulo').val();
+                var nome = $('#nome').val();
+                var sobrenome = $('#sobrenome').val();
+                var sufixo = $('#sufixo').val();
+
+                var nomeGerado = titulo + " " + nome + " " + sobrenome + " " + sufixo;
+                $('#rotulo').text(nomeGerado);
+            });
+
         $('#button-adicionar-nome').click(
             function () {
-                var tipoNome = $('#tipoNome').val();
-                var nome = $('#nome').val();
+                var nome = $('#rotulo').text();
+                var usoNome = $("#usoNome option:selected").text();
+                var nomePreferido = $("#nomePreferido option:selected").text();
+                var indicadorUsoCondicional = $("#indicadorUsoCondicional option:selected").text();
 
-                var html = '<tr>' +
-                '<td>' + tipoNome + '</td>' +
-                '<td>' + nome + '</td>';
-                
-                $('#tableNome').append(html);
+                if (nome != '' && !nome.includes('Preencha os campos abaixo')) {
+
+                    var html = '<tr>' +
+                        '<td>' + nome + '</td>' +
+                        '<td>' + usoNome + '</td>' +
+                        '<td>' + nomePreferido + '</td>' +
+                        '<td>' + indicadorUsoCondicional + '</td>' +
+                        + '</tr>';
+
+                    $('#tableNome').append(html);
+
+                }
             });
 
 
@@ -21,11 +43,12 @@ $(document).ready(
                 var areaGeografica = $("#areaGeografica option:selected").text();
 
                 var html = '<tr>' +
-                '<td>' + designacao + '</td>' +
-                '<td>' + emissor + '</td>' +
-                '<td>' + data + '</td>' +
-                '<td>' + areaGeografica + '</td>';
-                
+                    '<td>' + designacao + '</td>' +
+                    '<td>' + emissor + '</td>' +
+                    '<td>' + data + '</td>' +
+                    '<td>' + areaGeografica + '</td>'
+                    + '</tr>';
+
                 $('#tableIdentificador').append(html);
             });
 
