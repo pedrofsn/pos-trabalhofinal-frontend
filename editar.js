965aea4f62ca5c1d174685eb088f39240d77123e
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2017-10-28 18:21:52 -0200

    Cadastro da CTPS

--- editar.js
+++ editar.js
@@ -72,5 +72,18 @@ $(document).ready(
                 $('#tableCertidao').append(html);
             });
 
+        $('#button-adicionar-ctps').click(
+            function () {
+                var estadoCTPS = $("#estadoCTPS option:selected").text();
+                var serieCTPS = $('#serieCTPS').val();
+
+                var html = '<tr>' +
+                    '<td>' + serieCTPS + '</td>' +
+                    '<td>' + estadoCTPS + '</td>' +
+                    + '</tr>';
+
+                $('#tableCTPS').append(html);
+            });
+
     }
 );
