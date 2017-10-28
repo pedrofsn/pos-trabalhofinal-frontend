a76943ac2a447ae9bc6805c84fab0a4a4493d251
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2017-10-28 18:07:15 -0200

    Adicionando o tipo do identificador

--- editar.js
+++ editar.js
@@ -34,15 +34,16 @@ $(document).ready(
                 }
             });
 
-
         $('#button-adicionar-identificador').click(
             function () {
+                var tipoIdentificador = $("#tipoIdentificador option:selected").text();
                 var designacao = $('#designacao').val();
                 var emissor = $('#emissor').val();
                 var data = $('#data').val();
                 var areaGeografica = $("#areaGeografica option:selected").text();
 
                 var html = '<tr>' +
+                    '<td>' + tipoIdentificador + '</td>' +
                     '<td>' + designacao + '</td>' +
                     '<td>' + emissor + '</td>' +
                     '<td>' + data + '</td>' +
